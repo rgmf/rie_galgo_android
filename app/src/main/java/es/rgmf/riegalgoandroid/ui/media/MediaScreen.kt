a04412fec8bb728ffe094875a86784b9e8f9e7c9
Román Martínez <rgmf@riseup.net>
2024-08-22 21:54:41 +0200

    WIP: slider with zoom done.

--- app/src/main/java/es/rgmf/riegalgoandroid/ui/media/MediaScreen.kt
+++ app/src/main/java/es/rgmf/riegalgoandroid/ui/media/MediaScreen.kt
@@ -1,8 +1,8 @@
 package es.rgmf.riegalgoandroid.ui.media
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,24 +11,27 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.toSize
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import es.rgmf.riegalgoandroid.R
 import es.rgmf.riegalgoandroid.ui.PreferencesViewModel
+import kotlinx.coroutines.launch
+import kotlin.math.sqrt
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -41,46 +44,56 @@ fun MediaScreen(
     val token by prefsViewModel.tokenFlow.collectAsState(initial = "")
     val pagerState = rememberPagerState(pageCount = { ids.size }, initialPage = ids.indexOf(startId))
 
-    var scale by remember { mutableStateOf(1f) }
-    var offsetX by remember { mutableStateOf(0f) }
-    var offsetY by remember { mutableStateOf(0f) }
+    val coroutineScope = rememberCoroutineScope()
 
-    var boxSize by remember { mutableStateOf(Size.Zero) }
-    var imageSize by remember { mutableStateOf(Size.Zero) }
+    var scale by remember { mutableFloatStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
 
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .onGloballyPositioned { coordinates ->
-                boxSize = coordinates.size.toSize()
-            }
-            .pointerInput(Unit) {
-                detectTransformGestures { _, pan, zoom, _ ->
-                    scale = (scale * zoom).coerceAtLeast(1f)
-
-                    val currentImageSize = imageSize * scale
-
-                    val maxFactorX = ((scale * boxSize.width) - boxSize.width) / 2f
-                    val maxFactorY = (((scale * boxSize.height) - boxSize.height) / 2f)
-
-                    if (currentImageSize.width >= boxSize.width) {
-                        offsetX = (offsetX + pan.x).coerceIn(-maxFactorX, maxFactorX)
-                    } else {
-                        offsetX = (currentImageSize.width - imageSize.width) / 2f
-                    }
-
-                    if (currentImageSize.height >= boxSize.height) {
-                        offsetY = (offsetY + pan.y).coerceIn(-maxFactorY, maxFactorY)
-                    } else {
-                        offsetY = (currentImageSize.height - imageSize.height) / 2f
-                    }
-                }
-            }
-    ) {
+    Box(modifier = modifier.fillMaxSize()) {
         if (token.isNotEmpty()) {
             HorizontalPager(
                 state = pagerState,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .pointerInput(Unit) {
+                        coroutineScope.launch {
+                            awaitPointerEventScope {
+                                while (true) {
+                                    val event = awaitPointerEvent()
+                                    when (event.changes.size) {
+                                        2 -> {
+                                            val change1 = event.changes[0]
+                                            val change2 = event.changes[1]
+
+                                            val distanceCurrent = calculateDistance(
+                                                change1.position,
+                                                change2.position
+                                            )
+                                            val distancePrevious = calculateDistance(
+                                                change1.previousPosition,
+                                                change2.previousPosition
+                                            )
+                                            scale *= distanceCurrent / distancePrevious
+                                            offset += change1.positionChange()
+                                            Log.i("TAGTTTTTT",
+                                                "distanceCurrent: $distanceCurrent\ndistancePrevious: $distancePrevious\nscale: $scale\noffset: $offset"
+                                            )
+                                        }
+                                        else -> {
+                                            scale = 1f
+                                            offset = Offset.Zero
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offset.x,
+                        translationY = offset.y
+                    )
             ) { page ->
                 AsyncImage(
                     model = ImageRequest
@@ -93,17 +106,7 @@ fun MediaScreen(
                     placeholder = painterResource(R.drawable.loading_img),
                     contentDescription = stringResource(R.string.media_data_content_description),
                     contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .onGloballyPositioned { coordinates ->
-                            imageSize = coordinates.size.toSize()
-                        }
-                        .graphicsLayer(
-                            scaleX = scale,
-                            scaleY = scale,
-                            translationX = offsetX,
-                            translationY = offsetY
-                        )
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
         } else {
@@ -114,4 +117,15 @@ fun MediaScreen(
             )
         }
     }
+}
+
+/**
+ * Euclidean distance between two points in a 2D space.
+ */
+private fun calculateDistance(point1: Offset, point2: Offset): Float {
+    val dx = point1.x - point2.x
+    val dy = point1.y - point2.y
+
+    // Pythagorean theorem.
+    return sqrt(dx * dx + dy * dy)
 }
